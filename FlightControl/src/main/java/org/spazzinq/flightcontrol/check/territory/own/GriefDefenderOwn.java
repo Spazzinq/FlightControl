bc4b06e592fd1b5a41be82ff508db92221d99133
George <26337405+Spazzinq@users.noreply.github.com>
2021-01-30 18:26:54 -0500

    Add GriefDefender lib

---
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/GriefDefenderOwn.java
@@ -0,0 +1,13 @@
+package org.spazzinq.flightcontrol.check.territory.own;
+
+import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.claim.Claim;
+import com.griefdefender.api.claim.ClaimManager;
+import org.bukkit.entity.Player;
+import org.spazzinq.flightcontrol.check.territory.TerritoryCheck;
+
+public class GriefDefenderOwn extends TerritoryCheck {
+    @Override public boolean check(Player p) {
+        return GriefDefender.getCore().ge
+    }
+}
