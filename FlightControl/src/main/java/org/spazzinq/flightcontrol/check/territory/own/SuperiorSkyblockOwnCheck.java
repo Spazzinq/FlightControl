d0989c4b3e19e4973640cfb35808090f41f70129
George <26337405+Spazzinq@users.noreply.github.com>
2021-03-03 20:35:38 -0500

    SuperiorSkyblock NPE fix
    Because the developer of this plugin did not add that check...

--- FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/SuperiorSkyblockOwnCheck.java
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/SuperiorSkyblockOwnCheck.java
@@ -25,6 +25,7 @@
 package org.spazzinq.flightcontrol.check.territory.own;
 
 import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import org.bukkit.entity.Player;
 import org.spazzinq.flightcontrol.check.territory.TerritoryCheck;
@@ -32,7 +33,8 @@ import org.spazzinq.flightcontrol.check.territory.TerritoryCheck;
 public class SuperiorSkyblockOwnCheck extends TerritoryCheck {
     @Override public boolean check(Player p) {
         SuperiorPlayer player = SuperiorSkyblockAPI.getPlayer(p);
+        Island island = SuperiorSkyblockAPI.getIslandAt(p.getLocation());
 
-        return player != null && player.isInsideIsland();
+        return player != null && island != null && player.isInsideIsland();
     }
 }
