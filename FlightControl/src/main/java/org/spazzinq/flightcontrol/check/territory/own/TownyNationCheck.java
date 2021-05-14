5905eea5b2939a1060a073d0d233d34baf194dfd
George <26337405+Spazzinq@users.noreply.github.com>
2021-05-13 23:37:21 -0400

    Prevent Towny NPE
    Fixes #100

--- FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/TownyNationCheck.java
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/TownyNationCheck.java
@@ -25,6 +25,7 @@
 package org.spazzinq.flightcontrol.check.territory.own;
 
 import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
@@ -34,10 +35,10 @@ import org.spazzinq.flightcontrol.check.territory.TerritoryCheck;
 public class TownyNationCheck extends TerritoryCheck {
     @Override public boolean check(Player p) {
         try {
-            Resident resident = TownyAPI.getInstance().getDataSource().getResident(p.getName());
-            Town townAtLoc = TownyAPI.getInstance().getTownBlock(p.getLocation()).getTown();
+            Resident resident = TownyUniverse.getInstance().getResident(p.getName());
+            Town townAtLoc = TownyAPI.getInstance().getTown(p.getLocation());
 
-            if (resident.hasNation() && townAtLoc != null
+            if (resident != null && resident.hasNation() && townAtLoc != null
                     && resident.getTown().getNation().equals(townAtLoc.getNation())) {
                 return true;
             }
--- FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/TownyTownCheck.java
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/own/TownyTownCheck.java
@@ -25,6 +25,7 @@
 package org.spazzinq.flightcontrol.check.territory.own;
 
 import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
@@ -34,10 +35,10 @@ import org.spazzinq.flightcontrol.check.territory.TerritoryCheck;
 public final class TownyTownCheck extends TerritoryCheck {
     @Override public boolean check(Player p) {
         try {
-            Resident resident = TownyAPI.getInstance().getDataSource().getResident(p.getName());
-            Town townAtLoc = TownyAPI.getInstance().getTownBlock(p.getLocation()).getTown();
+            Resident resident = TownyUniverse.getInstance().getResident(p.getName());
+            Town townAtLoc = TownyAPI.getInstance().getTown(p.getLocation());
 
-            if (resident.hasTown() && resident.getTown().equals(townAtLoc)) {
+            if (resident != null && resident.hasTown() && resident.getTown().equals(townAtLoc)) {
                 return true;
             }
         } catch (NotRegisteredException ignored) {
