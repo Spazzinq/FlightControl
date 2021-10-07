ac30b23b0966fe29d9308827ad3b076c5ed5a0c7
George <26337405+Spazzinq@users.noreply.github.com>
2021-10-07 18:54:15 -0400

    Update copyright

--- FlightControl/src/main/java/org/spazzinq/flightcontrol/object/Timer.java
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/object/Timer.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of FlightControl, which is licensed under the MIT License.
  *
- * Copyright (c) 2020 Spazzinq
+ * Copyright (c) 2021 Spazzinq
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
