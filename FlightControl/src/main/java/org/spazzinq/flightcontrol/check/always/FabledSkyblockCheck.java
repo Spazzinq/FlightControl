98d72792cf7718c6f8105ef64406b330589e1ff1
George <26337405+Spazzinq@users.noreply.github.com>
2023-04-07 16:38:46 -0400

    Update FabledSkyblock check
    Fixes #264

--- FlightControl/src/main/java/org/spazzinq/flightcontrol/check/always/FabledSkyblockCheck.java
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/check/always/FabledSkyblockCheck.java
@@ -7,6 +7,7 @@ package org.spazzinq.flightcontrol.check.always;
 
 import com.songoda.skyblock.SkyBlock;
 import com.songoda.skyblock.island.Island;
+import com.songoda.skyblock.island.IslandManager;
 import com.songoda.skyblock.upgrade.Upgrade;
 import org.bukkit.entity.Player;
 import org.spazzinq.flightcontrol.api.object.Cause;
@@ -14,18 +15,16 @@ import org.spazzinq.flightcontrol.check.Check;
 
 public class FabledSkyblockCheck extends Check {
     @Override public boolean check(Player p) {
-        return getIsland(p).hasUpgrade(Upgrade.Type.Fly)
+        Island island = getManager().getIslandPlayerAt(p);
+
+        return island != null &&
+                (island.hasUpgrade(Upgrade.Type.Fly)
                 || p.hasPermission("fabledskyblock.fly.*")
-                || (p.hasPermission("fabledskyblock.fly") && getPlayerIsland(p) != null && getPlayerIsland(p).equals(getIsland(p)));
+                || (p.hasPermission("fabledskyblock.fly") && p.getUniqueId().equals(island.getOwnerUUID())));
     }
 
-    private Island getIsland(Player p) {
-        return SkyBlock.getInstance().getIslandManager().getIslandPlayerAt(p);
-    }
+    private IslandManager getManager() { return SkyBlock.getInstance().getIslandManager(); }
 
-    private Island getPlayerIsland(Player p) {
-        return SkyBlock.getInstance().getIslandManager().getIslandByOwner(p);
-    }
 
     @Override public Cause getCause() {
         return Cause.FABLED_SKYBLOCK_FLY;
