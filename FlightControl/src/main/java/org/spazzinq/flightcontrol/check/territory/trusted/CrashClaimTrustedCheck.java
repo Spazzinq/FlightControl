3b71b716e324ff09f203ae679fb5dea463b8d063
George <26337405+Spazzinq@users.noreply.github.com>
2023-05-29 23:31:53 -0400

    Update trusted CrashClaim check

--- FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/trusted/CrashClaimTrustedCheck.java
+++ FlightControl/src/main/java/org/spazzinq/flightcontrol/check/territory/trusted/CrashClaimTrustedCheck.java
@@ -6,19 +6,23 @@
 package org.spazzinq.flightcontrol.check.territory.trusted;
 
 import net.crashcraft.crashclaim.CrashClaim;
+import net.crashcraft.crashclaim.api.CrashClaimAPI;
+import net.crashcraft.crashclaim.claimobjects.Claim;
 import net.crashcraft.crashclaim.permissions.PermissionHelper;
 import net.crashcraft.crashclaim.permissions.PermissionRoute;
 import org.bukkit.entity.Player;
 import org.spazzinq.flightcontrol.check.territory.TerritoryCheck;
 
 public class CrashClaimTrustedCheck extends TerritoryCheck {
-    private PermissionHelper api;
+    private CrashClaimAPI api;
 
     @Override public boolean check(Player p) {
         if (api == null) {
-            api = CrashClaim.getPlugin().getApi().getPermissionHelper();
+            api = CrashClaim.getPlugin().getApi();
         }
 
-        return api.hasPermission(p.getLocation(), PermissionRoute.BUILD);
+        Claim claim = api.getClaim(p.getLocation());
+
+        return claim != null && claim.hasPermission(p.getUniqueId(), p.getLocation(), PermissionRoute.BUILD);
     }
 }
